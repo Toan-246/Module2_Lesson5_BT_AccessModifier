18bd4ab9bc99c61c1250080109946d70ddaaafcc
Toan-246 <n.v.toan687@gmail.com>
2022-01-10 17:43:09 +0700

    add

---
+++ src/com/codegym/Circle.java
@@ -0,0 +1,36 @@
+package com.codegym;
+
+public class Circle {
+    private double radius;
+    private String color;
+
+    public Circle() {
+        this.radius = 1.0;
+        this.color = "red";
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+        this.color = "red";
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
